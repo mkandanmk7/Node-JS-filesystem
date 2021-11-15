6c3571273c8865931d3a4bc9ff81d3995db130fa
muthu <mkandanmk7@gmail.com>
2021-11-15 20:15:57 +0530

    folder crreated

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ require("dotenv").config();
 
 const express = require("express");
 
+const fs = require("fs");
+
 const app = express();
 
 app.use(express.json());
 
-app.use("/", async (req, res) => {
+//creating a folder to store all files
+if (!fs.existsSync("Files"));
+
+fs.mkdirSync("Files");
+
+app.use((req, res) => {
   console.log("middle ware");
 });
 
